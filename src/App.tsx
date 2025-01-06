ea28fe8ad35d9a72f9f949935d37f598e1ea0b56
skmayya1 <skandamayya4@gmail.com>
2025-01-06 09:50:42 +0530

    tailwind and lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Lenis from "lenis";
+import { useEffect } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // Lenis
+  useEffect(() => {
+    console.log("App mounted")
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+    lenis.on('scroll', (e) => {
+      console.log(e);
+    });
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="h-screen w-full bg-[#FEFEFF]">
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="h-screen w-full bg-blue-500"></div>
+      <div className="h-screen w-full bg-green-500"></div>
+    </div>
   )
 }
 
